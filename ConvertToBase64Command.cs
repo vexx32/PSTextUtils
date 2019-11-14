92788c28482992cac22095dde52f031c3290826b
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-11-13 21:24:39 -0500

    :sparkles: Enhance ConvertTo-Base64
    - Improve input object handling
    
    - Add multiple possible input types

--- ConvertToBase64Command.cs
+++ ConvertToBase64Command.cs
@@ -1,32 +1,94 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Management.Automation;
+using System.Text;
 
 namespace PSTextUtils
 {
-    [Cmdlet(VerbsData.ConvertTo, "Base64String")]
+    [Cmdlet(VerbsData.ConvertTo, "Base64")]
     [OutputType(typeof(string))]
-    public class ConvertToBase64Command : Cmdlet
+    public class ConvertToBase64Command : PSCmdlet
     {
-        // Declare the parameters for the cmdlet.
-        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true)]
-        [Alias("InputString", "String", "Text")]
-        public string InputObject { get; set; }
+        private const string ObjectSet = "ObjectParameterSet";
+        private const string StringSet = "StringParameterSet";
 
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ParameterSetName = ObjectSet)]
+        public PSObject InputObject { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = StringSet)]
+        [Alias("String", "Text")]
+        public string InputString { get; set; }
 
         [Parameter]
         [ArgumentCompleter(typeof(EncodingCompleter))]
         [EncodingTransform]
-        public System.Text.Encoding Encoding { get; set; } = EncodingTransformAttribute.StandardEncoding["UTF8"];
+        public Encoding Encoding { get; set; } = EncodingTransformAttribute.StandardEncoding["UTF8"];
 
-
-        //This is the "Process {}" block of a Powershell script
+        /// <summary>
+        /// ProcessRecord() implementation for ConvertToBase64Command.
+        /// </summary>
         protected override void ProcessRecord()
         {
-            //WriteObject("Hello " + name + "!");
+            switch (ParameterSetName)
+            {
+                case ObjectSet:
+                    ProcessObject();
+                    break;
+                case StringSet:
+                    ProcessString();
+                    break;
+            }
+        }
 
-            byte[] bytes = Encoding.GetBytes(InputObject);
-            WriteObject(Convert.ToBase64String(bytes), true);
+        protected override void EndProcessing()
+        {
+            if (buffer.Count > 0)
+            {
+                WriteObject(Convert.ToBase64String(buffer.ToArray()));
+            }
+        }
 
+        private void ProcessString()
+        {
+            byte[] bytes = Encoding.GetBytes(InputString);
+            WriteObject(Convert.ToBase64String(bytes));
+        }
+
+        private readonly List<byte> buffer = new List<byte>();
+
+        private void ProcessObject()
+        {
+            if (InputObject.BaseObject is Stream data)
+            {
+                while (data.CanRead)
+                {
+                    buffer.Add((byte)data.ReadByte());
+                }
+
+                WriteObject(Convert.ToBase64String(buffer.ToArray()));
+                buffer.Clear();
+                return;
+            }
+
+            if (InputObject.BaseObject is FileInfo fileInfo)
+            {
+                WriteObject(Convert.ToBase64String(File.ReadAllBytes(fileInfo.FullName)));
+            }
+
+            if (LanguagePrimitives.TryConvertTo<byte>(InputObject.BaseObject, out byte byteValue))
+            {
+                buffer.Add(byteValue);
+                return;
+            }
         }
     }
 }
