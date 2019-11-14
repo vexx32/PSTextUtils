2b83b0e1244afc664a9feab53f9d586cf4ee4392
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-11-13 23:30:18 -0500

    :sparkles: Update ConvertFrom-Base64
    - Add direct to file and as byte outputs
    
    - Tidying and adding things

--- ConvertFromBase64Command.cs
+++ ConvertFromBase64Command.cs
@@ -1,31 +1,90 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Management.Automation;
+using System.Text;
 
 namespace PSTextUtils
 {
-    [Cmdlet(VerbsData.ConvertFrom, "Base64String")]
-    [OutputType(typeof(string))]
-    public class ConvertFromBase64Command : Cmdlet
+    [Cmdlet(VerbsData.ConvertFrom, "Base64")]
+    [OutputType(typeof(string), ParameterSetName = new[] { StandardSet })]
+    [OutputType(typeof(byte), ParameterSetName = new[] { ByteSet })]
+    public class ConvertFromBase64Command : PSCmdlet
     {
-        // Declare the parameters for the cmdlet.
-        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true)]
+        private const string StandardSet = "StandardSet";
+        private const string ByteSet = "ByteOutputSet";
+        private const string FileSet = "FileOutputSet";
+
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
         [Alias("InputString", "String", "Text")]
         public string InputObject { get; set; }
 
-
-        [Parameter]
+        [Parameter(ParameterSetName = StandardSet)]
         [ArgumentCompleter(typeof(EncodingCompleter))]
         [EncodingTransform]
-        public System.Text.Encoding Encoding { get; set; } = EncodingTransformAttribute.StandardEncoding["UTF8"];
+        public Encoding Encoding { get; set; } = EncodingTransformAttribute.StandardEncoding["UTF8"];
 
+        [Parameter(Mandatory = true, ParameterSetName = FileSet)]
+        [Alias("LiteralPath", "PSPath")]
+        public string Path { get; set; }
 
-        //This is the "Process {}" block of a Powershell script
+        [Parameter(Mandatory = true, ParameterSetName = ByteSet)]
+        public SwitchParameter ToBytes { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            if (ParameterSetName == FileSet)
+            {
+                if (WildcardPattern.ContainsWildcardCharacters(Path))
+                {
+                    var exception = new ArgumentException(
+                        "Non-literal paths are not supported by ConvertFrom-Base64.",
+                        nameof(Path));
+                    ThrowTerminatingError(new ErrorRecord(
+                        exception,
+                        "PSTextUtils.InvalidPath",
+                        ErrorCategory.InvalidArgument,
+                        Path));
+                }
+                else if (!InvokeProvider.Item.Exists(Path, force: false, literalPath: true))
+                {
+                    InvokeProvider.Item.New(Path, null, "File", null);
+                }
+            }
+        }
+
+        /// <summary>
+        /// ProcessRecord() implementation for ConvertFromBase64Command.
+        /// </summary>
         protected override void ProcessRecord()
         {
-            //WriteObject("Hello " + name + "!");
-            // $Encoding.GetString([System.Convert]::FromBase64String($Base64String))
-            byte[] bytes = Convert.FromBase64String(InputObject);
-            WriteObject(Encoding.GetString(bytes), true);
+            switch (ParameterSetName)
+            {
+                case StandardSet:
+                    byte[] bytes = Convert.FromBase64String(InputObject);
+                    WriteObject(Encoding.GetString(bytes));
+                    return;
+                case ByteSet:
+                    WriteObject(Convert.FromBase64String(InputObject), true);
+                    return;
+                case FileSet:
+                    fileBytes.AddRange(Convert.FromBase64String(InputObject));
+                    return;
+            }
         }
-    } //class
+
+        private readonly List<byte> fileBytes = new List<byte>();
+
+        /// <summary>
+        /// EndProcessing() implementation for ConvertFromBase64Command.
+        /// </summary>
+        protected override void EndProcessing()
+        {
+            if (ParameterSetName == FileSet)
+            {
+                var path = SessionState.Path.GetResolvedProviderPathFromPSPath(Path, out _)[0];
+                File.WriteAllBytes(path, fileBytes.ToArray());
+            }
+        }
+    }
 }
